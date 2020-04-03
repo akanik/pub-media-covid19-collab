d2c1764f794a13422ad095b60eaf5bb0d08d0056
Alexandra Kanik <alexandra@rationalact.com>
2020-04-03 10:33:31 -0700

    add cali js; didn't replace html due to discrepancies

--- app/site-specific/oh-app.js
+++ app/site-specific/oh-app.js
@@ -1,12 +1,11 @@
 var _$main_1 = {};
-//require("./lib/leaflet-knn.min");
 "use strict";
 
 //Values that will help your tracker look good when people search and share
-var meta_title = 'Kentucky COVID-19 County Tracker';
+var meta_title = 'Ohio COVID-19 County Tracker';
 var meta_twitter = '@OVReSRC';
-var meta_url = 'https://local.wfpl.org/covid19/kentucky-x.html';
-var meta_image = 'https://local.wfpl.org/covid19/assets/ky-covid-app-art.jpg';
+var meta_url = 'https://local.wfpl.org/covid19/ohio.html';
+var meta_image = 'https://local.wfpl.org/covid19/assets/oh-covid-app-art.jpg';
 var meta_description = 'EXPLORE: COVID-19-related cases and deaths in your county, as well as a risk assessment for your county’s population.';
 var meta_author = 'Alexandra Kanik';
 var meta_organization = 'Louisville Public Media';
@@ -20,22 +19,19 @@ var favicon120 = 'https://ohiovalleyresource.org/wp-content/themes/wfpl-v2/res/i
 var favicon76 = 'https://ohiovalleyresource.org/wp-content/themes/wfpl-v2/res/img/apple-touch-icon-76-precomposed.png';
 
 //Content tha tis specific to your state's tracker
-var banner_image = 'assets/ky-covid-header.jpg';
+var banner_image = 'assets/oh-covid-header.jpg';
 var data_sources = `
       <p>Age data provided by the 2018 5 year American Community Survey. Disease data are from 2014 and are provided by the <a href="http://ghdx.healthdata.org/us-data" target="_blank">Institute for Health Metrics and Evaluation's Global Health Data Exchange</a></p>
-      <p>As of March 28, 2020, case and death data for Kentucky counties are compiled from the Kentucky Governor's office daily COVID-19 data releases and from county health departments.</p>
-      <p>Case data are updated daily after the Governor's 5 p.m. new brief.</p>
-      <p>Historic data are provided by the <a href="https://github.com/nytimes/covid-19-data" target="_blank">New York Times county dataset</a>.</p>
-      <p>The data presented here only includes cases in which a person has tested positive for COVID-19, but it may be lower than the actual number of infected people in a community due to a lack of testing.</p> 
+      <p>*Case and death data for Ohio counties are compiled from the <a href="https://coronavirus.ohio.gov/wps/portal/gov/covid-19/home/dashboard" target='_blank'>Ohio Department of Health daily COVID-19 data release</a>. Case data for Ohio are one day behind. Data for yesterday are updated today at 2 p.m. ET on a daily basis.</p>
+      <p>In their data disclaimer, ODH notes: "All data displayed is preliminary and subject to change as more information is reported to ODH. Trends are based on the date of illness onset. If onset date is unknown, the earliest known date associated with the case is used as a substitute for the date of illness onset."</p>
+      <p>Preliminary case data may be lower than the actual number of infected people in a community due to a lack of testing or delayed reporting.</p> 
       <p>As counties report cases when test results come in, some of the increases in counties’ cases in one day could be due to many pending results coming in rather than many people coming down with symptoms in one day.</p>
       `;
       
-var lastUpdated = 'April 2, 5:30 p.m.';
-console.log(lastUpdated);
-var casesFile = 'assets/data/2020-04-02-Kentucky-export.csv';
-var stateFips = '21';
-var stateName = 'Kentucky';
-var statePop = 4440204;
-//State unknown county and/or unconfirmed cases
-var stateUKUC = 12;
+var lastUpdated = 'April 2, 2:00 p.m.*';
+var casesFile = 'assets/data/2020-04-02-Ohio-export.csv';
+var stateFips = '39';
+var stateName = 'Ohio';
+var statePop = 11641879;
 var dateFormat = "%Y-%m-%d";
+  
