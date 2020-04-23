d2d7498dd9765138659dbe85821f8ba88270551c
Emily Zentner <emily.zentner@capradio.org>
2020-04-23 08:29:55 -0700

    fixed data_sources

--- app/california/app.js
+++ app/california/app.js
@@ -15,7 +15,7 @@ var meta_org_logo = 'https://media.npr.org/assets/img/2019/06/17/nprlogo_rgb_whi
 var ga_id = '';
 
 //Content tha tis specific to your state's tracker
-var data_sources = `
+var dataSources = `
         <p>Current data on confirmed cases of COVID-19 and deaths due to the disease is provided by <a href="https://github.com/datadesk/california-coronavirus-data" target="_blank">the LA Times</a>. Historical data from the beginning of the pandemic through April 20, 2020 collected by Big Local News, USA Facts & staff from CapRadio, KQED and KPCC. This tracker will be updated every morning with the LA Times' data update from the previous night.</p>
         <p>While Alameda County and the city of Berkeley report their number of cases and deaths separately, we have added Berkeley’s totals into Alameda County’s totals for this data. Additionally, while Sutter and Yuba counties are one combined health jurisdiction, they sometimes report their data jointly, we have reported them separately here.</p>
         <p>The data presented here only includes cases in which a person has tested positive for COVID-19, but it may be lower than the actual number of infected people in a community due to a lack of testing. Not every county updates its numbers daily, so these totals reflect the latest numbers as of our last data collection.</p>
