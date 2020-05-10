2f65bbddbefabac3ddda7f5d52af8d171898175a
Emily Zentner <emily.zentner@capradio.org>
2020-05-10 09:16:38 -0700

    0509 ca data update
    ready to publish

--- app/california/app.js
+++ app/california/app.js
@@ -1,4 +1,4 @@
-var lastUpdated = 'May 8, 2020, 9:00 p.m';
+var lastUpdated = 'May 9, 2020, 9:00 p.m';
 var casesFile = 'data/ca-covid-data.csv';
 var dateFormat = '%m/%d/%Y';
 var stateName = 'California';
